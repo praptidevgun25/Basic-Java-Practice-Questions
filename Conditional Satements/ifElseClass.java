0030794ee4b1574c6a5ed28783259b5933dce27a
Prapti Devgun <praptidevgun25@gmail.com>
2021-06-27 01:00:47 +0530

    Create ifElseClass.java

---
+++ Conditional Satements/ifElseClass.java
@@ -0,0 +1,15 @@
+public class ifElseClass {
+
+	public static void main(String[] args) {
+		
+		int noOfPetals = 5;
+		
+		if(noOfPetals % 2 == 0) {
+			System.out.println("He loves me not");
+		} else {
+			System.out.println("He loves me");
+		}
+
+	}
+
+}
