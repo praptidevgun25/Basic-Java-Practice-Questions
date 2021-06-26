00fec458e3f35e07bc01eac82e5a9f59491128dd
Prapti Devgun <praptidevgun25@gmail.com>
2021-06-27 01:01:34 +0530

    Create ifElseIfClass.java

---
+++ Conditional Satements/ifElseIfClass.java
@@ -0,0 +1,26 @@
+public class ifElseIfClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		int number = 35;
+//		
+//		if(number <= 10) {
+//			System.out.println("Number is less than 10");
+//		} else if(number > 10 && number <=20) {
+//			System.out.println("Number is greater than 10 and less than 20");
+//		} else if(number > 20 && number <=30) {
+//			System.out.println("Number is greater than 20 and less than 30");
+//		} 
+		
+		int a = 15;
+		int b = 8;
+		
+		int maxOfBothNumbers = 0;
+		
+		maxOfBothNumbers = (a > b) ? a : b;
+		System.out.println(maxOfBothNumbers);
+
+	}
+
+}
