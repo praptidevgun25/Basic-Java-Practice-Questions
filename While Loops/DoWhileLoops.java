91294dd94fe4b4459f6d068c8ff1f8a960348b13
Prapti Devgun <praptidevgun25@gmail.com>
2021-06-27 01:45:48 +0530

    Create DoWhileLoops.java

---
+++ While Loops/DoWhileLoops.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class DoWhileLoops {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = 1;
+		do {
+			n = sc.nextInt();
+			System.out.println("number is "+n);
+		} while(n != 0);
+
+	}
+
+}
