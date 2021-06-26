d11b61a40c739eb79f9c2fded621815ff2d58101
Prapti Devgun <praptidevgun25@gmail.com>
2021-06-27 01:06:23 +0530

    Create ForLoops.java

---
+++ Loops/ForLoops.java
@@ -0,0 +1,16 @@
+public class ForLoops {
+
+	public static void main(String[] args) {
+		
+		int tableOf = 4;
+		int n = 0;
+		
+		for(int i = 1; i <= 10; i++) {
+			n = tableOf * i;
+			System.out.println(n);
+		
+		}
+
+	}
+
+}
