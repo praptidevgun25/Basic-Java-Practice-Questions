2aa1789a82ba6e010faf32f2334f5f6f036ed98b
Prapti Devgun <praptidevgun25@gmail.com>
2021-06-27 00:45:51 +0530

    Create ArrayIntro.java

---
+++ Arrays/ArrayIntro.java
@@ -0,0 +1,32 @@
+package arrays;
+
+public class ArrayIntro {
+
+	public static void main(String[] args) {
+		
+//		int[] marks = new int[5];
+		
+//		int[] marks;
+//		marks = new int[10];
+		
+//		int marks[] = new int[5];
+//		System.out.println(marks[4]);
+//		
+//		int[] rollNos, classes;
+		
+		int[] age = {2, 5, 1, 34, 12};
+		
+//		double[] marks = {1.0, 3.14, 2.9};
+//		
+//		marks[1] = 4.56;
+//		
+//		System.out.println(marks[1]);
+//		
+		for(int i = 0; i < age.length; i++) {
+			System.out.print(age[i] + " ");
+		}
+		
+		
+	}
+
+}
