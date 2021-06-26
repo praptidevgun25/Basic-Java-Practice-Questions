4a4a9ec9a1e05b49f913755379c2394478637cd9
Prapti Devgun <praptidevgun25@gmail.com>
2021-06-27 01:05:36 +0530

    Create Factorial.java

---
+++ Loops/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Factorial {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int fact = 1;
+		
+		for(int i = n ; i >= 1 ; i--) {
+			fact = fact * i;		
+		}
+		
+		System.out.println(fact);
+		
+	}
+
+}
