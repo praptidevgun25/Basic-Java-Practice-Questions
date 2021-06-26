580270a03927c7309e8e6e93bead539a2ef96916
Prapti Devgun <praptidevgun25@gmail.com>
2021-06-27 01:41:50 +0530

    Create LogicalOperators.java

---
+++ Operators/LogicalOperators.java
@@ -0,0 +1,13 @@
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		
+		int grade = 11 ;
+		
+		if(!(grade == 10 || grade == 12)) {
+			System.out.println("you can give board exams now.");
+		}
+
+	}
+
+}
