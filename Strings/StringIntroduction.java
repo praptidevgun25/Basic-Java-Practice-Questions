a0721c00b87f87f7961818b8489c10e5cf4e0e77
Prapti Devgun <praptidevgun25@gmail.com>
2021-06-27 01:43:14 +0530

    Create StringIntroduction.java

---
+++ Strings/StringIntroduction.java
@@ -0,0 +1,53 @@
+public class StringIntroduction {
+
+	public static void main(String[] args) {
+		
+//		String name = "Prapti Devgun";
+//		
+//		
+//		String anotherName = new String("Prapti");
+//		
+//		System.out.println(name.charAt(5));
+//		System.out.println(name.length());
+//		System.out.println(name.substring(3));
+//		System.out.println(name.substring(3, 12));
+//		System.out.println(name.contains("Prapti"));
+		 
+//		String name = "Prapti"; //string pool area
+//		String name2  = new String("Prapti"); //heap area
+//		
+//		System.out.println(name == name2);
+//		System.out.println(name.equals(name2)); 
+		
+//		String name = "";
+//		System.out.println(name.isEmpty());
+		
+		
+//		String name = "Prapti";
+//		name= name + "  Devgun";
+//		System.out.println(name);
+		
+//		String name = "Prapti";
+//		System.out.println(name.concat(" Devgun"));
+
+		
+//		String name = "Prapti";
+//		System.out.println(name.replace('P', 'A'));
+		
+		String cars = "Hyundai,Maruti,Swift,Wagonr,Ferrari,Lamborghini";
+		
+//		String allCars[] = cars.split(",");
+//		for(String car: allCars) {
+//			System.out.println(car);
+//		}
+		
+		String name = "PRapti";
+		System.out.println(name.indexOf('a'));
+		System.out.println(name.toLowerCase());
+		System.out.println(name.toUpperCase());
+		
+		String car = "    Bolt     ";
+		System.out.println(car.trim()); //removes aage peeche ki spaces
+	}
+
+}
