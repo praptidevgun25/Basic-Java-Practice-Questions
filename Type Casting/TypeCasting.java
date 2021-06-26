7bc8aa41870413b24a8401ac722d0526b3b44247
Prapti Devgun <praptidevgun25@gmail.com>
2021-06-27 01:44:30 +0530

    Create TypeCasting.java

---
+++ Type Casting/TypeCasting.java
@@ -0,0 +1,21 @@
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		int x = 2;
+//		
+//		double y = x;
+//		
+//		System.out.println(y);
+		
+		double myDouble = 2.8462;
+		
+		int myInt = (int)myDouble;
+		
+		System.out.println(myDouble);
+		System.out.println(myInt);
+
+	}
+
+}
