07deec8d483e3f35b680b0fe8c149212f5fc2690
Prapti Devgun <praptidevgun25@gmail.com>
2021-06-27 01:07:17 +0530

    Create NestedForLoop.java

---
+++ Loops/NestedForLoop.java
@@ -0,0 +1,14 @@
+public class NestedForLoop {
+
+	public static void main(String[] args) {
+		for(int j = 1; j <= 5; j++) {
+			for(int i = 1; i <= 10; i++) {
+			
+			System.out.print(i + " ");
+			
+			}
+		System.out.println();
+		}
+		}
+
+	}
