5e705f68ef3d09634fc17af39749a0e0680cb8d8
Prapti Devgun <praptidevgun25@gmail.com>
2021-06-27 01:00:11 +0530

    Create ifClass.java

---
+++ Conditional Satements/ifClass.java
@@ -0,0 +1,16 @@
+public class ifClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age = 19;
+		
+		if(age >= 18) {
+			System.out.println("you can vote now.");
+		}
+		
+		
+
+	}
+
+}
